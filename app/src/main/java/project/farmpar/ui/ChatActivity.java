b712c07178bc68bc5359911724095f31115e0d0e
warongrat <5735512049@psu.ac.th>
2018-01-27 01:50:25 +0700

    update

--- app/src/main/java/project/farmpar/ui/ChatActivity.java
+++ app/src/main/java/project/farmpar/ui/ChatActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -73,7 +74,10 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
 
         editWriteMessage = (EditText) findViewById(R.id.editWriteMessage);
         if (idFriend != null && nameFriend != null) {
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar1);
+            setSupportActionBar(toolbar);
             getSupportActionBar().setTitle(nameFriend);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
             recyclerChat = (RecyclerView) findViewById(R.id.recyclerChat);
             recyclerChat.setLayoutManager(linearLayoutManager);
